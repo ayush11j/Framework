ee79fef4ce6187c0db82fe74196ea779ff3a2bfa
ayushman mishra <amishra@akamai.com>
2018-08-11 22:42:52 +0530

    Adding the addition framework

---
+++ Addition.framework/Headers/Addition.h
@@ -0,0 +1,19 @@
+//
+//  Addition.h
+//  Addition
+//
+//  Created by Ayushman Mishra on 8/11/18.
+//  Copyright © 2018 Ayushman Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Addition.
+FOUNDATION_EXPORT double AdditionVersionNumber;
+
+//! Project version string for Addition.
+FOUNDATION_EXPORT const unsigned char AdditionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Addition/PublicHeader.h>
+
+
